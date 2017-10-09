13ca0a1c6b9f5faa6de4a828c19a99fc492982f6
Duknam Yoo <yoo2001818@gmail.com>
2017-10-09 23:58:56 +0900

    Add base structure for query simplifer

--- src/simplify.js
+++ src/simplify.js
@@ -1,16 +1,63 @@
+import * as ranges from './range';
+
 // Simplifies, or rather, serializes the Mongo-DB style query to a node
 // composed of operator type, queries, and children nodes.
 //
 // Since the database is not supposed to handle nested objects, i.e. pos.x,
 // it should change these formats into a single key separated by .: 'pos.x'.
 //
-const obj = {
-  type: 'and',
-  keys: {
-    a: [1, 2, 3],
-    b: [4, 5, 6],
-  },
-  children: [
 
-  ],
-};
+function joinName(names) {
+  return names.join('.');
+}
+
+export default function simplify(tree, names = [], type = 'and') {
+  if (typeof tree !== 'object') {
+    return ranges.eq([tree]);
+  }
+  let lastKey = null;
+  const keys = [];
+  const children = [];
+  for (let key in tree) {
+    switch (key) {
+      case '$eq':
+        break;
+      case '$gt':
+        break;
+      case '$gte':
+        break;
+      case '$in':
+        break;
+      case '$lt':
+        break;
+      case '$lte':
+        break;
+      case '$ne':
+        break;
+      case '$nin':
+        break;
+      case '$not':
+        break;
+      case '$nor':
+        break;
+      case '$and':
+        break;
+      case '$or':
+        break;
+      case '$exists':
+        break;
+      case '$type':
+        break;
+      case '$all':
+        break;
+      case '$elemMatch':
+        break;
+      case '$size':
+        break;
+      default:
+        lastKey = key;
+        break;
+    }
+  }
+  return { type, keys, children };
+}
