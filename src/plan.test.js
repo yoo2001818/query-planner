ae1f8b822c7bc442a56fbe031665d6f8ab97913e
Duknam Yoo <yoo2001818@gmail.com>
2017-12-04 23:49:27 +0900

    Fully expand console log output

--- src/plan.test.js
+++ src/plan.test.js
@@ -1,33 +1,38 @@
 import plan from './plan';
 import simplify from './simplify';
+import util from 'util';
 
 describe('plan', () => {
   it('should use index for simple queries', () => {
-    console.log(plan(simplify({ a: 5, b: 3 }), null, [
+    console.log(util.inspect(plan(simplify({ a: 5, b: 3 }), null, [
       ['a', 'b'],
       ['a'],
-    ]));
+    ]), false, null));
   });
   it('should use index for simple queries along with sort', () => {
-    console.log(plan(simplify({ a: 5, b: 3 }), [['c', 1]], [
+    console.log(util.inspect(plan(simplify({ a: 5, b: 3 }), [['c', 1]], [
       ['a', 'b', 'c'],
       ['a', 'b'],
       ['a'],
-    ]));
+    ]), false, null));
   });
   it('should use index for or queries', () => {
-    console.log(plan(simplify({ $or: [{ a: 5, b: 3 }, { a: 1, b: 7 }] }), null, [
-      ['a', 'b'],
-      ['a'],
-    ]));
+    console.log(util.inspect(plan(
+      simplify({ $or: [{ a: 5, b: 3 }, { a: 1, b: 7 }] }), null, [
+        ['a', 'b'],
+        ['a'],
+      ]), false, null,
+    ));
   });
   it('should use index for sorting', () => {
-    console.log(plan(simplify({ $or: [{ a: 5, b: 3 }, { a: 1, b: 7 }] }), [
-      ['c', 1],
-    ], [
-      ['a', 'b', 'c'],
-      ['a', 'b'],
-      ['a'],
-    ]));
+    console.log(util.inspect(plan(
+      simplify({ $or: [{ a: 5, b: 3 }, { a: 1, b: 7 }] }), [
+        ['c', 1],
+      ], [
+        ['a', 'b', 'c'],
+        ['a', 'b'],
+        ['a'],
+      ]), false, null,
+    ));
   });
 });
