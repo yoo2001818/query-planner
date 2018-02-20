bd71510d789987fd9a854d016cd17f66d1ad4fff
Duknam Yoo <yoo2001818@gmail.com>
2018-02-20 23:52:53 +0900

    Add clause table tests

---
+++ src/clauseTable.js
@@ -0,0 +1,8 @@
+// Accepts a SQL expression and builds a 2D clause table for it.
+
+export default function makeClauseTable(where) {
+  // Traverse and generate bitset positions - this is used to generate actual
+  // bitmap.
+  console.log(where);
+  return {};
+}
---
+++ src/clauseTable.test.js
@@ -0,0 +1,57 @@
+import parse from 'yasqlp';
+import makeClauseTable from './clauseTable';
+
+function clauseFromSQL(query) {
+  return makeClauseTable(parse(query)[0].where);
+}
+
+function getArrayResult(query) {
+  let result = clauseFromSQL(query);
+  return { clauses: result.clauses, bitmap: Array.from(result.iterator) };
+}
+
+describe('makeClauseTable', () => {
+  it('should handle a single expression', () => {
+    expect(getArrayResult('SELECT * WHERE a = \'1\';')).toEqual({
+      clauses: [{
+        type: 'compare',
+        op: '=',
+        left: { type: 'column', table: null, name: 'a' },
+        right: { type: 'string', value: '1' },
+      }],
+      bitmap: [1],
+    });
+  });
+  it('should handle a AND expression', () => {
+    expect(getArrayResult('SELECT * WHERE a = \'1\' AND b = 3;')).toEqual({
+      clauses: [{
+        type: 'compare',
+        op: '=',
+        left: { type: 'column', table: null, name: 'a' },
+        right: { type: 'string', value: '1' },
+      }, {
+        type: 'compare',
+        op: '=',
+        left: { type: 'column', table: null, name: 'b' },
+        right: { type: 'number', value: 3 },
+      }],
+      bitmap: [3],
+    });
+  });
+  it('should handle a OR expression', () => {
+    expect(getArrayResult('SELECT * WHERE a = \'1\' OR b = 3;')).toEqual({
+      clauses: [{
+        type: 'compare',
+        op: '=',
+        left: { type: 'column', table: null, name: 'a' },
+        right: { type: 'string', value: '1' },
+      }, {
+        type: 'compare',
+        op: '=',
+        left: { type: 'column', table: null, name: 'b' },
+        right: { type: 'number', value: 3 },
+      }],
+      bitmap: [1, 2],
+    });
+  });
+});
