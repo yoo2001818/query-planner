9df868a222b01fc63a68dcc50ef8f17576bfee2d
Duknam Yoo <yoo2001818@gmail.com>
2018-02-22 23:55:40 +0900

    Make clause traversal to callback style

--- src/clauseTable.js
+++ src/clauseTable.js
@@ -4,25 +4,27 @@ export default function makeClauseTable(where) {
   // Traverse and generate bitset positions - this is used to generate actual
   // bitmap.
   let clauses = [];
-  let indexedWhere = traverseClauses(where, clauses);
+  let indexedWhere = traverseClauses(where, where => {
+    let self = Object.assign({}, where, { index: clauses.length });
+    clauses.push(self);
+    return self;
+  });
   return { clauses };
 }
 
-function traverseClauses(where, output) {
+function traverseClauses(where, callback) {
   console.log(where);
   if (where.type === 'logical') {
     if (where.op === '||' || where.op === '&&') {
       return Object.assign({}, where, {
-        values: where.values.map(v => traverseClauses(v, output)),
+        values: where.values.map(v => traverseClauses(v, callback)),
       });
     }
   }
   if (where.type === 'unary' && where.op === '!') {
     return Object.assign({}, where, {
-      value: traverseClauses(where.value, output),
+      value: traverseClauses(where.value, callback),
     });
   }
-  let self = Object.assign({}, where, { index: output.length });
-  output.push(self);
-  return self;
+  return callback(where);
 }
