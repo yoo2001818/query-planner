66aea23e639c68fa79bb5049cd1d6ed27fd91f5e
Duknam Yoo <yoo2001818@gmail.com>
2018-03-02 23:55:16 +0900

    Change bitset generator to use JS generator

--- src/clauseTable.js
+++ src/clauseTable.js
@@ -1,38 +1,29 @@
 // Accepts a SQL expression and builds a 2D clause table for it.
 
 export default function makeClauseTable(where) {
-  const result = generateBitsets(where, []);
-  return { iterator: result.bitmap, clauses: result.clauses };
+  const result = generateBitsets(where);
+  return { iterator: result, clauses: result.clauses };
 }
 
-function generateBitsets(where, clauses) {
+function * generateBitsets(where) {
   if (where.type === 'logical') {
     if (where.op === '||') {
-      return where.values.reduce(
-        ({ bitmap, clauses }, v) => {
-          const result = generateBitsets(v, clauses);
-          return {
-            bitmap: bitmap.concat(result.bitmap),
-            clauses: result.clauses,
-          };
-        }, { bitmap: [], clauses });
+      for (let entry of where.values) {
+        yield * generateBitsets(entry);
+      }
     } else if (where.op === '&&') {
       // Perform a cartesian product between values
-      return where.values.reduce(({ bitmap, clauses }, v) => {
-        const result = generateBitsets(v, clauses);
-        if (bitmap.length === 0) return result;
-        let outputBitmap = [];
-        for (let i = 0; i < bitmap.length; ++i) {
+      yield * where.values.reduce(function * (p, v) {
+        const result = Array.from(generateBitsets(v));
+        if (p == null) return yield * result;
+        for (let entry of p) {
           for (let j = 0; j < result.bitmap.length; ++j) {
-            outputBitmap.push(bitmap[i] | result.bitmap[j]);
+            yield entry | result.bitmap[j];
           }
         }
-        return { bitmap: outputBitmap, clauses: result.clauses };
-      }, { bitmap: [], clauses });
+      }, null);
     }
+  } else {
+    yield 1 << where.index;
   }
-  return {
-    bitmap: [1 << clauses.length],
-    clauses: clauses.concat(where),
-  };
 }
