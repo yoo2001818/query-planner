80af5a09bbca7ea58dbd33f8108482914eb8a25d
Duknam Yoo <yoo2001818@gmail.com>
2018-03-09 23:57:40 +0900

    Add simplify test code

--- src/simplify.test.js
+++ src/simplify.test.js
@@ -1,108 +1,48 @@
-import * as ranges from './range';
+import parse from 'yasqlp';
+
 import simplify from './simplify';
 
+function getAST(query) {
+  return parse(query)[0].where;
+}
+
 describe('simplify', () => {
-  it('should parse simple query', () => {
-    expect(simplify({
-      a: 5,
-      b: [1, 3],
-    })).toEqual({
-      isAnd: true,
-      keys: {
-        a: ranges.eq([5]),
-        b: ranges.eq([1, 3]),
-      },
-      children: [],
-    });
+  it('should handle a single expression', () => {
+    expect(simplify(getAST('SELECT * WHERE a = \'1\';')))
+      .toEqual(getAST('SELECT * WHERE a = \'1\';'));
   });
-  it('should parse range query', () => {
-    expect(simplify({
-      a: { $gt: 9, $lte: 30 },
-      b: { $in: [10, 15] },
-    })).toEqual({
-      isAnd: true,
-      keys: {
-        a: ranges.range(9, 30, false, true),
-        b: ranges.eq([10, 15]),
-      },
-      children: [],
-    });
+  it('should simplify NOT expression', () => {
+    expect(simplify(getAST('SELECT * WHERE NOT(a = \'1\');')))
+      .toEqual(getAST('SELECT * WHERE a != \'1\';'));
   });
-  it('should parse or query', () => {
-    expect(simplify({
-      a: { $or: [5, 3] },
-      b: { $not: { $or: [5, 3] } },
-    })).toEqual({
-      isAnd: true,
-      keys: {
-        a: ranges.eq([5, 3]),
-        b: ranges.not(ranges.eq([5, 3])),
-      },
-      children: [],
-    });
+  it('should simplify OR expression', () => {
+    expect(simplify(getAST('SELECT * WHERE a = \'1\' OR a > \'1\';')))
+      .toEqual(getAST('SELECT * WHERE a >= \'1\';'));
+    expect(simplify(getAST('SELECT * WHERE a < \'1\' OR a > \'1\';')))
+      .toEqual(getAST('SELECT * WHERE a != \'1\';'));
+    expect(simplify(getAST('SELECT * WHERE a < \'1\' OR a >= \'1\';')))
+      .toEqual(getAST('SELECT * WHERE TRUE;'));
   });
-  it('should remove redundant block', () => {
-    expect(simplify({
-      $or: [{ b: 19 }, { b: 13 }],
-    })).toEqual({
-      isAnd: true,
-      keys: {
-        b: ranges.eq([19, 13]),
-      },
-      children: [],
-    });
+  it('should simplify AND expression', () => {
+    expect(simplify(getAST('SELECT * WHERE a > 10 AND a > 20;')))
+      .toEqual(getAST('SELECT * WHERE a > 20;'));
+    expect(simplify(getAST('SELECT * WHERE a >= 10 AND a <= 10;')))
+      .toEqual(getAST('SELECT * WHERE a = 10;'));
+    expect(simplify(getAST('SELECT * WHERE a > 10 AND a < 10;')))
+      .toEqual(getAST('SELECT * WHERE FALSE;'));
   });
-  it('should parse or query with multiple keys', () => {
-    expect(simplify({
-      $or: [{ a: 99 }, { b: 13 }],
-    })).toEqual({
-      isAnd: false,
-      keys: {
-        a: ranges.eq([99]),
-        b: ranges.eq([13]),
-      },
-      children: [],
-    });
+  it('should simplify compound NOT expression', () => {
+    expect(simplify(getAST('SELECT * WHERE !(a = 1 AND b = 2);')))
+      .toEqual(getAST('SELECT * WHERE a != 1 OR b != 2;'));
+    expect(simplify(getAST('SELECT * WHERE !(a = 1 OR b = 2);')))
+      .toEqual(getAST('SELECT * WHERE a != 1 AND b != 2;'));
   });
-  it('should parse not', () => {
-    expect(simplify({
-      $not: {
-        a: { $gt: 9, $lte: 30 },
-        b: { $in: [10, 15] },
-      },
-    })).toEqual({
-      isAnd: false,
-      keys: {
-        a: ranges.range(30, 9, true, false),
-        b: ranges.neq([10, 15]),
-      },
-      children: [],
-    });
+  it('should simplify between expression', () => {
+    expect(simplify(getAST('SELECT * WHERE a BETWEEN 1 AND 2;')))
+      .toEqual(getAST('SELECT * WHERE a <= 1 AND a <= 2;'));
   });
-  it('should merge redundant same keys', () => {
-    expect(simplify({
-      $or: [{ a: 1, b: 5 }, { b: 6 }, { b: 7 }, { c: 8 }],
-    })).toEqual({
-      isAnd: false,
-      keys: {
-        b: ranges.eq([6, 7]),
-        c: ranges.eq([8]),
-      },
-      children: [{
-        isAnd: true,
-        keys: {
-          a: ranges.eq([1]),
-          b: ranges.eq([5]),
-        },
-        children: [],
-      }],
-    });
+  it('should simplify in expression', () => {
+    expect(simplify(getAST('SELECT * WHERE a IN (1, 2, 3);')))
+      .toEqual(getAST('SELECT * WHERE a = 1 OR a = 2 OR a = 3;'));
   });
-  /*
-  it('should merge keys using distributive property', () => {
-    console.log(inspect(simplify({
-      $or: [{ a: 1, b: 5 }, { a: 1, b: 6 }],
-    }), false, null));
-  });
-  */
 });
